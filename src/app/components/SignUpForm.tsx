6a1d41b605a427a48562e162ff980e84aa36fc93
itsbluejelly <ronniedenzel0@gmail.com>
2024-01-29 02:52:04 +0300

    added zod to form

--- src/app/components/SignUpForm.tsx
+++ src/app/components/SignUpForm.tsx
@@ -26,29 +26,36 @@ const FormSchema = z
       .min(2, "First name must be atleast 2 characters")
       .max(45, "First name must be less than 45 characters")
       .regex(new RegExp("^[a-zA-Z]+$"), "No special character allowed!"),
+
     lastName: z
       .string()
       .min(2, "Last name must be atleast 2 characters")
       .max(45, "Last name must be less than 45 characters")
       .regex(new RegExp("^[a-zA-Z]+$"), "No special character allowed!"),
+
     email: z.string().email("Please enter a valid email address"),
+
     phone: z
       .string()
       .refine(validator.isMobilePhone, "Please enter a valid phone number!"),
+
     password: z
       .string()
       .min(6, "Password must be at least 6 characters ")
       .max(50, "Password must be less than 50 characters"),
+
     confirmPassword: z
       .string()
       .min(6, "Password must be at least 6 characters ")
       .max(50, "Password must be less than 50 characters"),
+
     accepted: z.literal(true, {
       errorMap: () => ({
         message: "Please accept all terms",
       }),
     }),
   })
+  
   .refine((data) => data.password === data.confirmPassword, {
     message: "Password and confirm password doesn't match!",
     path: ["confirmPassword"],
@@ -57,34 +64,31 @@ const FormSchema = z
 type InputType = z.infer<typeof FormSchema>;
 
 const SignUpForm = () => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    control,
-    watch,
-    formState: { errors },
-  } = useForm<InputType>({
+  const { register, handleSubmit, reset, control, watch, formState: { errors }} = useForm<InputType>({
     resolver: zodResolver(FormSchema),
   });
+
   const [passStrength, setPassStrength] = useState(0);
   const [isVisiblePass, setIsVisiblePass] = useState(false);
 
   useEffect(() => {
     setPassStrength(passwordStrength(watch().password).id);
   }, [watch]);
+  
   const toggleVisblePass = () => setIsVisiblePass((prev) => !prev);
 
   const saveUser: SubmitHandler<InputType> = async (data) => {
     const { accepted, confirmPassword, ...user } = data;
-    try {
+    
+    try{
       // const result = await registerUser(user);
       toast.success("The User Registered Successfully.");
-    } catch (error) {
+    }catch (error) {
       toast.error("Something Went Wrong!");
       console.error(error);
     }
   };
+
   return (
     <form
       onSubmit={handleSubmit(saveUser)}
@@ -97,6 +101,7 @@ const SignUpForm = () => {
         label="First Name"
         startContent={<UserIcon className="w-4" />}
       />
+      
       <Input
         errorMessage={errors.lastName?.message}
         isInvalid={!!errors.lastName}
@@ -104,6 +109,7 @@ const SignUpForm = () => {
         label="Last Name"
         startContent={<UserIcon className="w-4" />}
       />
+      
       <Input
         errorMessage={errors.email?.message}
         isInvalid={!!errors.email}
@@ -112,6 +118,7 @@ const SignUpForm = () => {
         label="Email"
         startContent={<EnvelopeIcon className="w-4" />}
       />{" "}
+      
       <Input
         errorMessage={errors.phone?.message}
         isInvalid={!!errors.phone}
@@ -120,6 +127,7 @@ const SignUpForm = () => {
         label="Phone"
         startContent={<PhoneIcon className="w-4" />}
       />{" "}
+      
       <Input
         errorMessage={errors.password?.message}
         isInvalid={!!errors.password}
@@ -128,6 +136,7 @@ const SignUpForm = () => {
         label="Password"
         type={isVisiblePass ? "text" : "password"}
         startContent={<KeyIcon className="w-4" />}
+        
         endContent={
           isVisiblePass ? (
             <EyeSlashIcon
@@ -142,7 +151,9 @@ const SignUpForm = () => {
           )
         }
       />
+      
       {/* <PasswordComponent passStrength={passStrength} /> */}
+      
       <Input
         errorMessage={errors.confirmPassword?.message}
         isInvalid={!!errors.confirmPassword}
@@ -152,6 +163,7 @@ const SignUpForm = () => {
         type={isVisiblePass ? "text" : "password"}
         startContent={<KeyIcon className="w-4" />}
       />
+      
       <Controller
         control={control}
         name="accepted"
@@ -165,13 +177,11 @@ const SignUpForm = () => {
           </Checkbox>
         )}
       />
-      {!!errors.accepted && (
-        <p className="text-red-500">{errors.accepted.message}</p>
-      )}
+
+      {!!errors.accepted && <p className="text-red-500">{errors.accepted.message}</p>}
+
       <div className="flex justify-center col-span-2">
-        <Button className="w-48" color="primary" type="submit">
-          Submit
-        </Button>
+        <Button className="w-48" color="primary" type="submit">Submit</Button>
       </div>
     </form>
   );
